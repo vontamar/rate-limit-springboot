58e2baff78e9e4bcdb08658920462faf28896a46
youssefelya <youssefelyamani96@gmail.com>
2020-10-27 13:17:04 +0100

    rate limit example

--- src/main/java/com/example/ratelimit/demo/controller/ProductController.java
+++ src/main/java/com/example/ratelimit/demo/controller/ProductController.java
@@ -2,8 +2,10 @@ package com.example.ratelimit.demo.controller;
 
 
 import com.example.ratelimit.demo.data.entity.Product;
-import com.google.common.util.concurrent.RateLimiter;
+import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -12,23 +14,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Date;
 
 @RestController
-@RequestMapping("/products")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
 
-    private final Bucket bucket;
 
-    @GetMapping()
-    public ResponseEntity<List<Product>> getAllProducts() {
+    public Bucket createNewBucket() {
+        long capacity = 10;
+        Refill refill = Refill.greedy(10, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(capacity, refill);
+        return Bucket4j.builder().addLimit(limit).build();
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(HttpServletRequest httpRequest ) {
+        HttpSession session = httpRequest.getSession(true);
+        String appKey =String.valueOf("<Security Key> ");  //SecurityUtils.getThirdPartyAppKey();
+        Bucket bucket = (Bucket) session.getAttribute("throttler-" + appKey);
+        if (bucket == null) {
+            bucket = createNewBucket();
+            session.setAttribute("throttler-" + appKey, bucket);
+        }
         boolean okToGo = bucket.tryConsume(1);
-
         if (okToGo) {
             return new ResponseEntity<List<Product>>(
                    Arrays.asList(new Product("product 1", "category 1", new Date()),
